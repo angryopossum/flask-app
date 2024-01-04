c4759990b3c721b6d67aa376ad4057f1c3b69bc2
-= AngryOpossum =- <angryopossum@protonmail.com>
2024-01-04 12:43:02 +0000

    add ip

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import socket
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '<h1>Hello, World!</h1>'
+def ip():
+    ip_address = socket.gethostbyname(socket.gethostname())
+    return f"IP-адрес клиента: {ip_address}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
