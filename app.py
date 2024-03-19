dd80478dd7cd90ad9f0717f7babcbe994e5e7087
-= AngryOpossum =- <angryopossum@protonmail.com>
2024-03-19 12:18:47 +0000

    change port in app

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def ip():
     return f"IP-адрес клиента: {ip_address}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
