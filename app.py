5334aa1da0697bb0b8f0e1abc6b4c975bf5bd597
-= AngryOpossum =- <angryopossum@protonmail.com>
2024-05-15 12:02:46 +0000

    add env

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import socket
+import socket,os
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,6 +10,13 @@ def home():
 def ip():
     ip_address = socket.gethostbyname(socket.gethostname())
     return f"IP-адрес клиента: {ip_address}"
+@app.route('/env')
+def env():
+    env_vars = os.environ
+    env_vars_str = "<br>".join([f"{key}: {value}" for key, value in env_vars.items()])
+    return f"Environment variables:<br>{env_vars_str}"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
